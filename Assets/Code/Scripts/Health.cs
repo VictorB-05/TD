936f958c3ed4835d9765529677f176ddbc34c1b3
victor <victorbejar2005@gmail.com>
2025-01-27 19:36:29 +0100

    Construir torres y dinero
    Se ha implementado construir una torre y dinero

--- Assets/Code/Scripts/Health.cs
+++ Assets/Code/Scripts/Health.cs
@@ -6,12 +6,17 @@ public class Health : MonoBehaviour
 {
     [Header("Atributes")]
     [SerializeField] private int hitPoints = 2;
+    [SerializeField] private int currencyWorth = 20;
+
+    private bool isDestroyed=false;
 
     public void TakeDamage(int dmg) {
         hitPoints -= dmg;    
 
-        if (hitPoints <= 0) {
+        if (hitPoints <= 0 && !isDestroyed) {
             EnemySpawner.onEnemyDestroy.Invoke();
+            LevelManager.main.increaseCurrency(currencyWorth);
+            isDestroyed = true;
             Destroy(gameObject);
         }
     }
--- Assets/Code/Scripts/LevelManager.cs
+++ Assets/Code/Scripts/LevelManager.cs
@@ -3,14 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelManager : MonoBehaviour {
+
     public static LevelManager main;
+
     public Transform startPoint;
     public Transform[] path;
+
+    public int currency;
+
     private void Awake() {
         main = this;
     }
 
+    private void Start() {
+        currency = 100;
+    }
+
     public Transform getPath(int index) {
         return path[index];
+    }
+
+    public void increaseCurrency(int amount) {
+        currency += amount;
+    }
+
+    public bool SpendCurrency(int amount) {
+        if (amount <= currency) {
+            currency -= amount;
+            return true;
+        } else {
+            Debug.Log("Falta dinero para comprar");
+            return false;
+        }
     }
 }
